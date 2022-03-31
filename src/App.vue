7e4b077605a1bd3b0f1670304ebac72c03500ebc
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-03-31 14:06:59 +0700

    ex1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img :alt="imageAlt" :src="image" />
+    <div>{{ product }}</div>
+    <a :href="productLink">Link</a>
+    <div :style="{ color: activeColor }">{{ price }}</div>
+    <div>tien sau giam gia: {{money()}}</div>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      image: 'https://cdn.vjshop.vn/hightech/may-choi-game/ps5/sony-ps-5-1.jpg',
+      imageAlt: 'ps5 alt',
+      product: 'ps5',
+      productLink: 'https://bachtungps.com.vn/may-sony-playstation-5-ps5-1',
+      quantity: 10,
+      price: 10000,
+      discount: 0.2,
+      inStock: true,
+      classObject: {
+        'bg-green': true,
+        'bg-blue': false
+      }
+    }
+  },
+  methods: {
+    money() {
+      const priceDiscount = this.price - (this.price * this.discount)
+      return priceDiscount
+    }
+  },
+  computed: {
+   
   }
 }
 </script>
@@ -24,5 +53,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  width: 300px;
+  height: 300px;
+}
+.text-red {
+  color: red;
 }
 </style>
