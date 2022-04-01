707d0bd2daca731de98d4b5c055d64d2cc04e05d
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-04-01 10:31:01 +0700

    session2-bai1

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,99 @@
 <template>
-  <div id="app">
-    <img src="https://images.unsplash.com/photo-1648655878148-49440bff2109?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxM3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="">
-    <img src="https://images.unsplash.com/photo-1648547568862-69bed7121073?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="">
-    <img src="https://images.unsplash.com/photo-1648629629660-efb2e6ee2459?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60" alt="">
-    <img src="https://images.unsplash.com/photo-1628442222245-d40cfce76fd1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="">
-  </div>
+    <div id="app">
+        <p>Danh dach san pham</p>
+        <div v-for="(item, index) in products" :key="index">
+            <p>{{item.name}}</p>
+            <p>{{item.price}}</p>
+            <p>{{item.publicDate}}</p>
+            <img :src="item.image" alt="">
+            <a @click="message('them vao gio hang')" href="">Them vao gio hang</a>
+        </div>
+        <p>San pham hot</p>
+        <div v-for="(item, index) in hotProducts" :key="index">
+            <p>{{item.name}}</p>
+            <p>{{item.price}}</p>
+            <p>{{item.publicDate}}</p>
+            <img :src="item.image" alt="">
+            <a @click="message('them vao gio hang')" href="">Them vao gio hang</a>
+        </div>
+
+        <p>Phuong thuc thanh toan</p>
+        <div v-for="(item,index) in paymentMethods " :key="index">
+            {{item.text}}
+        </div>
+    </div>
 </template>
 
 <script>
-
-
-export default {
-  name: 'App',
-  components: {
-
-  }
-}
+    export default {
+        name: "App",
+        components: {},
+        data() {
+            return {
+                cart: [],
+                products: [
+                    {
+                        id: 1,
+                        name: "PS5",
+                        price: 5555,
+                        publicDate: "05-05-2021",
+                        image:
+                            "https://cdn.vjshop.vn/hightech/may-choi-game/ps5/sony-ps-5-1.jpg",
+                        hot: true,
+                    },
+                    {
+                        id: 2,
+                        name: "PS4",
+                        price: 4444,
+                        publicDate: "04-04-2021",
+                        image:
+                            "https://gmedia.playstation.com/is/image/SIEPDC/ps4-slim-image-block-01-en-24jul20?$native--t$",
+                        hot: true,
+                    },
+                    {
+                        id: 3,
+                        name: "PS3",
+                        price: 3333,
+                        publicDate: "03-03-2021",
+                        image:
+                            "https://game.haloshop.vn/image/catalog/blogs/ps3-co-con-dang-mua/ps3-co-con-dang-mua-21.jpg",
+                        hot: false,
+                    },
+                ],
+                paymentMethods: [
+                    {text: "COD", value: 1},
+                    {text: "Banking", value: 2},
+                    {text: "Ứng dụng bên thứ 3", value: 3},
+                ],
+                selectedPayment: 2,
+            };
+        },
+        methods: {
+            message: function (mess) {
+                alert(mess)
+            }
+        },
+        computed: {
+            hotProducts() {
+                return this.products.filter(product => product.hot);
+            },
+        },
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
 
-img {
-  width: 200px;
-  height: 200px;
-  margin: 10px;
-}
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    img {
+        width: 200px;
+        height: 200px;
+        margin: 10px;
+    }
 </style>
