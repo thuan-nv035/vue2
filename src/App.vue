c639d2825f5c5207aa292170613d3561f3f998eb
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-04-06 11:44:07 +0700

    ex2

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
-  <div id="app">
-   </div>
+    <div id="app">
+        <div>
+            <myButton></myButton>
+            <custom-button >
+
+            </custom-button>
+        </div>
+    </div>
 </template>
 
 <script>
+    export default {
+        name: 'App',
+        components: {
 
+        },
+        data(){
+            return{
+                link: 'abc',
+            }
+        }
 
-export default {
-  name: 'App',
-  components: {
-
-  }
-}
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.component('custom-button',{
+  // props:{
+  //   href: String,
+  // },
+  data(){
+    return {
+    }
+  },
+  template: '<button ><a >Helloooo</a></button>'
+
+})
+
+Vue.component(  'myButton',  {
+  data() {
+    return {
+      buttonText: ' myButton'
+    }
+  },
+  template:' <button>{{buttonText}} </button>'
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
