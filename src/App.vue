2d18e8ba42f5e8bcdd605aedd3d666182117ed74
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-04-06 13:41:18 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="app">
-      <myButton></myButton>
-      <mySelect></mySelect>
-      <ParentComponent></ParentComponent>
-   </div>
+    <div id="app">
+        <myButton></myButton>
+        <mySelect></mySelect>
+        <ParentComponent></ParentComponent>
+
+        <custom-button :href="link" >
+
+        </custom-button>
+    </div>
 </template>
 
 <script>
 
-import ParentComponent from "./components/ParentComponent"
-export default {
-  name: 'App',
-  components: {
-      ParentComponent
-  }
-}
+    import ParentComponent from "./components/ParentComponent"
+
+    export default {
+        name: 'App',
+        components: {
+            ParentComponent
+        },
+        data() {
+            return {
+                link: 'abc',
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -15,6 +15,20 @@ Vue.component( 'mySelect', {
   template: '<select><option>Vui Lòng chọn</option></select>'
 })
 
+Vue.component('custom-button',{
+  props:{
+    href: String,
+  },
+  components: {
+
+  },
+  data(){
+    return {
+    }
+  },
+  template: `<button ><a :href="href">Helloooo</a></button>`
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
