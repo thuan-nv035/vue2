f625a5a2a75cdfa596edecf44e87acafb4fde448
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-04-07 13:50:29 +0700

    404 not found

--- src/pages/ListProduct.vue
+++ src/pages/ListProduct.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <div v-for="(item,index) in [1,2,3,4,5]" :key="index">
-            <router-link :to="{name:'detail-product',query:{id:item,slug:'sp'+item}}">{{item}}</router-link>
+            <router-link :to="{name:'detail-product',params:{id:item,name:'sp'+item}}">{{item}}</router-link>
+<!--            <router-link :to="{name:'detail-product',query:{id:item,slug:'sp'+item}}">{{item}}</router-link>-->
         </div>
     </div>
 </template>
--- src/pages/ProductDetail.vue
+++ src/pages/ProductDetail.vue
@@ -3,6 +3,7 @@
         <div>CHi tiết sp</div>
         {{productId}}
         {{slug}}
+        {{name}}
     </div>
 </template>
 
@@ -13,11 +14,13 @@
             return {
                 productId: 0,
                 slug: ''
+                // name: ''
             }
         },
         mounted() {
             this.productId = this.$route.query.id;
-            this.slug = this.$route.query.slug;
+            // this.slug = this.$route.query.slug;
+            this.name = this.$route.query.name;
         }
     }
 </script>
